00d881a0732481bf07234323acdfacb724086db2
Siyana Trendafilova <bebovska@gmail.com>
2024-11-06 20:14:06 +0200

    add examples - tags

--- djangoTemplatesBasics/djangoTemplatesBasics/posts/urls.py
+++ djangoTemplatesBasics/djangoTemplatesBasics/posts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from djangoTemplatesBasics.posts import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('dashboard/', views.dashboard, name='dash')
 ]
--- djangoTemplatesBasics/djangoTemplatesBasics/posts/views.py
+++ djangoTemplatesBasics/djangoTemplatesBasics/posts/views.py
@@ -14,5 +14,32 @@ def index(request):
         "IDs": ["1223", "exe1234", "si5684"],
         "some_text": "everything works well!",
         "no_text": "",
+        "users": ["pesho", "ivan", "stamat", "maria", "magdalena"]
     }
     return render(request, 'base.html', context)
+
+def dashboard(request):
+    context = {
+        "posts": [
+            {
+                "title": "How to create Django project",
+                "author": "Maria Kirilova",
+                "content": "Follow these steps:...",
+                "created_at": datetime.now(),
+            },
+            {
+                "title": "How to create HTML file ",
+                "author": "Ivan Abadjiev",
+                "content": "It is the most easiest thing to do",
+                "created_at": datetime.now(),
+            },
+            {
+                "title": "How to create CSS file",
+                "author": "",
+                "content": "You should follow my steps",
+                "created_at": datetime.now(),
+            },
+        ]
+    }
+
+    return render(request, 'posts/dashboard.html', context)
