eb45ab473fad001ccb14664d7fd5f5eef0570537
Siyana Trendafilova <bebovska@gmail.com>
2024-11-06 17:44:52 +0200

    add examples

--- djangoTemplatesBasics/djangoTemplatesBasics/posts/views.py
+++ djangoTemplatesBasics/djangoTemplatesBasics/posts/views.py
@@ -6,6 +6,10 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "current_time": datetime.now()
+        "current_time": datetime.now(),
+        "person": {
+            "age": 20,
+            "height": 1.90,
+        }
     }
     return render(request, 'base.html', context)
