d753b4da538024cd1127f5f3d080c77f23e58b2e
Siyana Trendafilova <bebovska@gmail.com>
2024-11-06 17:36:13 +0200

    add solutions

--- djangoTemplatesBasics/djangoTemplatesBasics/posts/views.py
+++ djangoTemplatesBasics/djangoTemplatesBasics/posts/views.py
@@ -1,5 +1,11 @@
+from datetime import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'base.html')
+    context = {
+        "current_time": datetime.now()
+    }
+    return render(request, 'base.html', context)
