1fa0d882a64f924aa749e94dc791233e3dc0975d
Siyana Trendafilova <bebovska@gmail.com>
2024-12-09 00:28:37 +0200

    feat: Created bool & choice field in forms.py

--- formsBasics/forumApp/forumApp/posts/forms.py
+++ formsBasics/forumApp/forumApp/posts/forms.py
@@ -1,9 +1,22 @@
 from django import forms
 class PersonForm(forms.Form):
+    STATUS_CHOICES = (
+        (1, 'Draft'),
+        (2, 'Published'),
+        (3, 'Archived')
+    )
     person_name = forms.CharField(
-        label='Put your first name',
+        label='First name',
+        help_text='Put your first name',
         max_length=10,
     )
     age = forms.IntegerField(
-        label='Put your age'
+        help_text='Put your age'
+    )
+    is_lecturer = forms.BooleanField(
+        label='Check if you are lecturer'
+    )
+
+    status = forms.ChoiceField(
+        choices=STATUS_CHOICES,
     )
