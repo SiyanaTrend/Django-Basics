9df1df93f0d42453a17bf684aa43c4dda469fa2f
Siyana Trendafilova <bebovska@gmail.com>
2024-12-09 21:12:09 +0200

    feat: Added more versions of validator

--- formsBasics/forumApp/forumApp/posts/validators.py
+++ formsBasics/forumApp/forumApp/posts/validators.py
@@ -1,17 +1,63 @@
 from django.core.exceptions import ValidationError
 from django.utils.deconstruct import deconstructible
 
+# version 1
+# @deconstructible
+# class BadLanguageValidator:
+#     def __init__(self, bad_words=None, message=None):
+#         self.bad_words = bad_words or ['pig', 'hippo']
+#         self.message = message or 'The text contains bad language!'
+#
+#     def __call__(self, value):
+#         for bad_word in self.bad_words:
+#             if bad_word.lower() in value.lower():
+#                 raise ValidationError(self.message)
 
+# version 2
 @deconstructible
 class BadLanguageValidator:
+    def __init__(self, bad_words=None, message=None):
+        self.bad_words = bad_words
+        self.message = message or 'The text contains bad language!'
 
-    def __init__(self, bad_words=None):
-        if bad_words is None:
-            self.bad_words = ['pig', 'hippo']
+    @property
+    def bad_words(self):
+        return self.__bad_words
+
+    @bad_words.setter
+    def bad_words(self, value):
+        if value is None:
+            self.__bad_words = ['pig', 'hippo']
         else:
-            self.bad_words = bad_words
+            self.__bad_words = value
 
     def __call__(self, value):
-        for bad_word in self.bad_words:
-            if bad_word.lower() in value.lower():
-                raise ValidationError('The text contains bad language!')
+        if any(bad_word.lower() in value.lower() for bad_word in self.bad_words): # using a generator expression within any()
+            raise ValidationError(self.message)
+
+
+# version 3
+    # prevent misusage of code like, when bad_words is set to 'pig' (a string): validators = BadLanguageValidator('pig').
+    # when run the project ValueError would raise in terminal with the message
+# @deconstructible
+# class BadLanguageValidator:
+#     def __init__(self, bad_words=None, message=None):
+#         self.bad_words = bad_words
+#         self.message = message or 'The text contains bad language!'
+#
+#     @property
+#     def bad_words(self):
+#         return self.__bad_words
+#
+#     @bad_words.setter
+#     def bad_words(self, value):
+#         if value is None:
+#             self.__bad_words = ['pig', 'hippo']
+#         elif not isinstance(value, list):               # ensure bad_words is always a list
+#             raise ValueError("bad_words must be a list of strings.")
+#         else:
+#             self.__bad_words = value
+#
+#     def __call__(self, value):
+#         if any(bad_word.lower() in value.lower() for bad_word in self.bad_words):
+#             raise ValidationError(self.message)
