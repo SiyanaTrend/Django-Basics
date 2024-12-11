65bfa0d699ba3c9cc3a0a6123140fe1a0e6c95f9
Siyana Trendafilova <bebovska@gmail.com>
2024-12-11 23:21:06 +0200

    feat: Added examples for simple form styling
    see index.html

--- formsAdvanced/forumApp/forumApp/settings.py
+++ formsAdvanced/forumApp/forumApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'forumApp.posts.apps.PostsConfig'
 ]
 
@@ -114,6 +115,7 @@ USE_I18N = True
 USE_TZ = True
 
 CSRF_COOKIE_SECURE = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
